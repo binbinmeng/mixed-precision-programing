03cb7f0db35314c9a60a92997b28e45a12c8076e
binbinmeng <15221657504@163.com>
2018-05-14 10:25:49 +0800

    update

--- p100/p100_sgemm.cc
+++ p100/p100_sgemm.cc
@@ -9,55 +9,9 @@
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
 #include "fp16_conversion.h"
+#include "../util/util.h"
 
 using namespace std;
-
-const char* cublasGetErrorString(cublasStatus_t status)
-{
-    switch(status)
-    {
-        case CUBLAS_STATUS_SUCCESS: return "CUBLAS_STATUS_SUCCESS";
-        case CUBLAS_STATUS_NOT_INITIALIZED: return "CUBLAS_STATUS_NOT_INITIALIZED";
-        case CUBLAS_STATUS_ALLOC_FAILED: return "CUBLAS_STATUS_ALLOC_FAILED";
-        case CUBLAS_STATUS_INVALID_VALUE: return "CUBLAS_STATUS_INVALID_VALUE"; 
-        case CUBLAS_STATUS_ARCH_MISMATCH: return "CUBLAS_STATUS_ARCH_MISMATCH"; 
-        case CUBLAS_STATUS_MAPPING_ERROR: return "CUBLAS_STATUS_MAPPING_ERROR";
-        case CUBLAS_STATUS_EXECUTION_FAILED: return "CUBLAS_STATUS_EXECUTION_FAILED"; 
-        case CUBLAS_STATUS_INTERNAL_ERROR: return "CUBLAS_STATUS_INTERNAL_ERROR"; 
-    }
-    return "unknown error";
-}
-
-// Convenience function for checking CUDA runtime API results
-// can be wrapped around any runtime API call. No-op in release builds.
-inline
-cudaError_t checkCuda(cudaError_t result)
-{
-  if (result != cudaSuccess) {
-    fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
-    assert(result == cudaSuccess);
-  }
-  return result;
-}
-
-inline
-cublasStatus_t checkCublas(cublasStatus_t result)
-{
-  if (result != CUBLAS_STATUS_SUCCESS) {
-    fprintf(stderr, "CUDA Runtime Error: %s\n", cublasGetErrorString(result));
-    assert(result == CUBLAS_STATUS_SUCCESS);
-  }
-  return result;
-}
-
-// Fill the array A(nr_rows_A, nr_cols_A) with random numbers on CPU
-void CPU_fill_rand(float *A, int nr_rows_A, int nr_cols_A) {
-	int a=1;
-
-    for(int i = 0; i < nr_rows_A * nr_cols_A; i++){
-		A[i] = (float)rand()/(float)(RAND_MAX/a);
-	}
-}
 
 int main(int argc, char ** argv){
 
@@ -121,7 +75,7 @@ int main(int argc, char ** argv){
     lda = m;
     ldb = k;
     ldc = m;
-    
+
     cudaEventRecord(start, 0);
 
 
