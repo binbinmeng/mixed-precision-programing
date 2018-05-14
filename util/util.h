5a8d85c1035f8babee9f33297b3f072b17b7e369
binbinmeng <15221657504@163.com>
2018-05-14 10:38:26 +0800

    add utile

--- util/util.h
+++ util/util.h
@@ -4,5 +4,56 @@
 
 #ifndef MIXED_PRECISION_UTIL_H
 #define MIXED_PRECISION_UTIL_H
+#include <unistd.h>
+#include <iostream>
+#include <stdlib.h>
+#include <assert.h>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+const char* cublasGetErrorString(cublasStatus_t status)
+{
+    switch(status)
+    {
+        case CUBLAS_STATUS_SUCCESS: return "CUBLAS_STATUS_SUCCESS";
+        case CUBLAS_STATUS_NOT_INITIALIZED: return "CUBLAS_STATUS_NOT_INITIALIZED";
+        case CUBLAS_STATUS_ALLOC_FAILED: return "CUBLAS_STATUS_ALLOC_FAILED";
+        case CUBLAS_STATUS_INVALID_VALUE: return "CUBLAS_STATUS_INVALID_VALUE";
+        case CUBLAS_STATUS_ARCH_MISMATCH: return "CUBLAS_STATUS_ARCH_MISMATCH";
+        case CUBLAS_STATUS_MAPPING_ERROR: return "CUBLAS_STATUS_MAPPING_ERROR";
+        case CUBLAS_STATUS_EXECUTION_FAILED: return "CUBLAS_STATUS_EXECUTION_FAILED";
+        case CUBLAS_STATUS_INTERNAL_ERROR: return "CUBLAS_STATUS_INTERNAL_ERROR";
+    }
+    return "unknown error";
+}
 
+// Convenience function for checking CUDA runtime API results
+// can be wrapped around any runtime API call. No-op in release builds.
+inline
+cudaError_t checkCuda(cudaError_t result)
+{
+    if (result != cudaSuccess) {
+        fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+        assert(result == cudaSuccess);
+    }
+    return result;
+}
+
+inline
+cublasStatus_t checkCublas(cublasStatus_t result)
+{
+    if (result != CUBLAS_STATUS_SUCCESS) {
+        fprintf(stderr, "CUDA Runtime Error: %s\n", cublasGetErrorString(result));
+        assert(result == CUBLAS_STATUS_SUCCESS);
+    }
+    return result;
+}
+
+// Fill the array A(nr_rows_A, nr_cols_A) with random numbers on CPU
+void CPU_fill_rand(float *A, int nr_rows_A, int nr_cols_A) {
+    int a=1;
+
+    for(int i = 0; i < nr_rows_A * nr_cols_A; i++){
+        A[i] = (float)rand()/(float)(RAND_MAX/a);
+    }
+}
 #endif //MIXED_PRECISION_UTIL_H
